aa844c59d2323f4ac2681456c5d2eb57782fac41
Federico Jordan <federico.jordan@pedidosya.com>
2019-09-25 11:18:00 -0300

    First commit

---
+++ IDDTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDDTodoList
+//
+//  Created by Federico Jordan on 24/09/2019.
+//  Copyright © 2019 PedidosYa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
